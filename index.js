4b68a900eb0373d4c94e6e8e5adf5702ad0526c2
Derick Yang <derick.yang@tufts.edu>
2016-04-13 19:52:04 -0400

    some styling. voting inside images

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ var mongoUri = process.env.MONGOLAB_URI || "mongodb://localhost:27017/dining";
 var MongoClient = require('mongodb').MongoClient;
 var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
     db = databaseConnection;
+    app.set('port', (process.env.PORT || 5000));
+    app.listen(app.get('port'), function() {
+        console.log('Node app is running on port', app.get('port'));
+    });
 });
 
 var weights = {
@@ -50,7 +54,6 @@ var weights = {
 
 var tddAPI = 'https://tuftsdiningdata.herokuapp.com/menus/';
 
-app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -347,7 +350,7 @@ function checkForFood(foodType, foodname, callback) {
         if (!result) {
             var toAdd = {
                 name: foodname,
-                imgurl: 'http://placehold.it/200/eeba93?text=No+Image+Found',
+                imgurl: 'http://placehold.it/400/eeba93?text=No+Image+Found',
                 type: foodType,
                 weight: weights[foodType], //delete this?
                 up: Math.floor((Math.random() + Math.random() + Math.random()) * 10),
@@ -361,6 +364,3 @@ function checkForFood(foodType, foodname, callback) {
     });
 }
 
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-});
--- public/js/init.js
+++ public/js/init.js
@@ -8,112 +8,129 @@ window.addEventListener('load', function() {
     $.ajax({
         url: '/getmealdata',
         data: {
-            meal: "Dinner",
+            meal: 'Dinner',
             day: 28,
             month: 3,
             year: 2016
         },
         success: function(result) {
+            carmResults = result.carm.score;
+            dewickResults = result.dewick.score;
 
-                // INTERMEDIATE STEP: 
-                // will be real scores eventually
-                // result.carm.score = 400;
-                // result.dewick.score = 625;
-                //
-                //
-                //
+            loadBar();
 
-                carmResults = result.carm.score;
-                dewickResults = result.dewick.score;
+            console.log(result);
+            // sort in order of most significant
+            result.carm.food_arr.sort(sortFoods);
+            result.dewick.food_arr.sort(sortFoods);
 
-                loadBar();
-
-                console.log(result);
-                // sort in order of most significant
-                result.carm.food_arr.sort(sortFoods);
-                result.dewick.food_arr.sort(sortFoods);
-
-                result.carm.food_arr.forEach(function(item, index) {
-                    $("#leftfoods").append(function() {
-                        return $("<div>").append(function() {
-                            return $("<img>").attr({
-                                alt: item.name,
-                                src: item.imgurl,
-                                class: 'left-food-thumbnail'
-                            });
-                        }).append(function() {
-                            return $("<span>").append(function() {
-                                return $("<div>").html(item.name).attr({
-                                    class: 'food-title'
-                                });
-                            }).append(function() { // add carm upvote
-                                var carmUPvoteDIV = $("<div id=upvote>").html("<button type='button' class='btn btn-success upButton'>√ " + item.up + "</button>");
-                                carmUPvoteDIV.click(function(){
-                                    item.up++;
-                                    // item in database = item.up
-                                    carmUPvoteDIV.html("<button type='button' class='btn btn-success upButton'>√ " + item.up + "</button>");
-                                });
-                                return carmUPvoteDIV;
-                            }).append(function() { // add carm downvote
-                                var carmDOWNvoteDIV = $("<div id=downvote>").html("<button type='button' class='btn btn-danger downButton'>X " + item.down+"</button>");
-                                carmDOWNvoteDIV.click(function(){
-                                    item.down++;
-                                    // item in database = item.down
-                                    carmDOWNvoteDIV.html("<button type='button' class='btn btn-danger downButton'>X "+ item.down+"</button>");
-                                });
-                                return carmDOWNvoteDIV;
-                            }).attr({
-                                class: 'food-desc'
-                            });
-                        });
-                    }).attr({
-                        class: 'food-unit'
-                    });
+            result.carm.food_arr.forEach(function(item, index) {
+                $('#leftfoods').append(function() {
+                    return createItem(item, 'left');
                 });
-                // sort in order of most significant
-                result.dewick.food_arr.forEach(function(item, index) {
-                    $("#rightfoods").append(function() {
-                        return $("<div>").append(function() {
-                            return $("<img>").attr({
-                                    alt: item.name,
-                                    src: item.imgurl,
-                                    class: 'right-food-thumbnail'
-                                });
-                            }).prepend(function() {
-                                return $("<span>").append(function() {
-                                    return $("<div>").html(item.name).attr({
-                                        class: 'food-title'
-                                    });
-                                }).append(function() { // add dewick up vote
-                                    var dewickUPvoteDIV = $("<div id=upvote>").html("<button type='button' class='btn btn-success upButton'>√ " + item.up + "</button>");
-                                    dewickUPvoteDIV.click(function(){
-                                        item.up++;
-                                        // item in database = item.up
-                                        dewickUPvoteDIV.html("<button type='button' class='btn btn-success upButton'>√ " + item.up + "</button>");
-                                    });
-                                    return dewickUPvoteDIV;
-                                }).append(function() { // add dewick down vote
-                                    var dewickDOWNvoteDIV = $("<div id=downvote>").html("<button type='button' class='btn btn-danger downButton'>X " + item.down+"</button>");
-                                    dewickDOWNvoteDIV.click(function(){
-                                        item.down++;
-                                        // item in database = item.down
-                                        dewickDOWNvoteDIV.html("<button type='button' class='btn btn-danger downButton'>X " + item.down+"</button>");
-                                    });
-                                    return dewickDOWNvoteDIV;
-                                }).attr({
-                                    class: 'food-desc'
-                                });
-                            });
-                        }).attr({
-                            class: 'food-unit'
-                        });
+            });
+            // sort in order of most significant
+            result.dewick.food_arr.forEach(function(item, index) {
+                $('#rightfoods').append(function() {
+                    return createItem(item, 'right');
                 });
-            }
+            });
+        }
     });
 });
 
 function sortFoods(a, b) {
-    return ((b.up - b.down) * b.weight) - ((a.up - a.down) * a.weight);        
+    return ((b.up - b.down) * b.weight) - ((a.up - a.down) * a.weight);
+}
+
+function createItem(item, position) {
+    var opp = 'right';
+    if (position === 'right') {
+        opp = 'left';
+    }
+    return $('<div>').append(function() {
+        return $('<div>').attr({
+            class: 'desc'
+        }).append(function() {
+            return $('<span>').html(item.name).attr({
+                class: 'food-title ' + position + '-align ' + position + '-block'
+            });
+        }).append(function() {
+            return $('<div>').append(function() {
+                return $('<button>').attr({
+                    class: 'btn btn-success'
+                }).append(function() {
+                    return $('<span>').attr({
+                        class: 'glyphicon glyphicon-menu-up'
+                    });
+                });
+            }).append(function() {
+                return $('<button>').attr({
+                    class: 'btn btn-failure'
+                }).append(function() {
+                    return $('<span>').attr({
+                        class: 'glyphicon glyphicon-menu-down'
+                    });
+                });
+            }).attr({
+                class: 'voting pull-' + opp
+            });
+        });
+    }).attr({
+        class: 'food-block',
+        style: 'background-image: url(' + item.imgurl +');'
+    }).append({
+
+    });
+    // .css('background-image',
+    //     'url(' + item.imgurl + ')').html(item.name).attr({
+    //     // class: 'img-thumbnail food-item'
+    // });
+    /*.append(function() {
+        return $('<div>').append(function() {
+            return $('<div>').html(item.name).attr({
+                class: 'food-title'
+            });
+        }).append(function() {
+            return $('<button>').attr({
+                class: 'btn btn-success'
+            });
+        }).append(function() {
+            return $('<button>').attr({
+                class: 'btn btn-failure'
+            });
+        }).attr({
+            class: 'food-desc'
+        });
+    }).attr({
+        class: 'food-block'
+    });*/
+
+
+    // }).append(function() {
+    //     return $('<span>').append(function() {
+    //         return $('<div>').html(item.name).attr({
+    //             class: 'food-title'
+    //         }).append(function() { // add carm upvote
+    //             var carmUPvoteDIV = $('<div id=upvote>').html('<button type='button' class='btn btn-success upButton'>√ ' + item.up + '</button>');
+    //             carmUPvoteDIV.click(function() {
+    //                 item.up++;
+    //                 // item in database = item.up
+    //                 carmUPvoteDIV.html('<button type='button' class='btn btn-success upButton'>√ ' + item.up + '</button>');
+    //             });
+    //             return carmUPvoteDIV;
+    //         }).append(function() { // add carm downvote
+    //             var carmDOWNvoteDIV = $('<div id=downvote>').html('<button type='button' class='btn btn-danger downButton'>X ' + item.down + '</button>');
+    //             carmDOWNvoteDIV.click(function() {
+    //                 item.down++;
+    //                 // item in database = item.down
+    //                 carmDOWNvoteDIV.html('<button type='button' class='btn btn-danger downButton'>X ' + item.down + '</button>');
+    //             });
+    //             return carmDOWNvoteDIV;
+    //         });
+    //     });
+    // });
+
 }
 
 // React.js: call this.setState() in react. check also: componentDidMount
